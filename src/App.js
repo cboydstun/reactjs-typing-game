b9f291eb4f044212964aee0cba895a4fced320fd
Chris Boydstun <chrisboydstun@gmail.com>
2021-10-08 18:54:57 -0500

    init commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import TypingGameComponent from './components/TypingGameComponent'
+
+export default function App() {
+  return (
+    <div>
+        <TypingGameComponent />
+    </div>
+  )
+}
---
+++ src/components/TypingGameComponent.js
@@ -0,0 +1,40 @@
+import React from 'react';
+// import it
+import useTypingGame from 'react-typing-game-hook';
+
+const TypingGameComponent = () => {
+  // Call the hook
+  const {
+    states: { chars, charsState },
+    actions: { insertTyping, resetTyping, deleteTyping },
+  } = useTypingGame('The heroes are awakening! Click here to begin.');
+
+  // Capture and display!
+  return (
+    <h1
+      onKeyDown={e => {
+        const key = e.key;
+        if (key === 'Escape') {
+          resetTyping();
+        } else if (key === 'Backspace') {
+          deleteTyping(false);
+        } else if (key.length === 1) {
+          insertTyping(key);
+        }
+        e.preventDefault();
+      }}
+      tabIndex={0}
+    >
+      {chars.split('').map((char, index) => {
+        let state = charsState[index];
+        let color = state === 0 ? 'black' : state === 1 ? 'green' : 'red';
+        return (
+          <span key={char + index} style={{ color }}>
+            {char}
+          </span>
+        );
+      })}
+    </h1>
+  );
+};
+export default TypingGameComponent;
